47212945c626f15e7f2776cbedf4498d3a23c0bf
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2023-07-02 01:04:44 +0530

    helper change

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
 // export const BASE_URI = "https://social-minds-backend.onrender.com"
-export const BASE_URI = "https://social-minds-backend-production.up.railway.app/"
+export const BASE_URI = "https://social-minds-backend-production.up.railway.app"
 // export const BASE_URI = "http://localhost:3001"
